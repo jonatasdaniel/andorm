02f397b6108b18d6774f2a2f55516002865fd5bb
jonatasdaniel <jonatas.hermann@gmail.com>
2011-03-04 14:01:00 -0300

    PersistenceManager and Factory added. Must refactor PersistenceManager javadoc

---
+++ main/src/br/com/andorm/persistence/PersistenceManagerFactory.java
@@ -0,0 +1,16 @@
+package br.com.andorm.persistence;
+
+import br.com.andorm.AndOrmConfiguration;
+
+
+public final class PersistenceManagerFactory {
+	
+	private PersistenceManagerFactory() {}
+	
+	public static PersistenceManager create(AndOrmConfiguration configuration) {
+		PersistenceManager manager = null;
+		
+		return manager;
+	}
+
+}
