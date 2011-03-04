5996c319a4845f72f4906ddff76306a7f713e6ee
jonatasdaniel <jonatas.hermann@gmail.com>
2011-03-04 14:00:11 -0300

    Configuration and Exception added

---
+++ main/src/br/com/andorm/AndOrmConfiguration.java
@@ -0,0 +1,41 @@
+package br.com.andorm;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import resources.ResourceBundleFactory;
+
+/**
+ * 
+ * @author jonatasdaniel
+ * @since 04/03/2011
+ * @version 0.1
+ *
+ */
+public class AndOrmConfiguration {
+
+	private final String databasePath;
+	private final List<Class<?>> entities;
+	private final ResourceBundle bundle = ResourceBundleFactory.get();
+	
+	public AndOrmConfiguration(String databasePath) {
+		entities = new ArrayList<Class<?>>();
+		if(databasePath != null)
+			this.databasePath = databasePath;
+		else
+			throw new AndOrmException(bundle.getString("invalid_database_path"));
+	}
+	
+	public void addEntity(Class<?> entityClass) {
+		entities.add(entityClass);
+	}
+	
+	public String getDatabasePath() {
+		return databasePath;
+	}
+	
+	public List<Class<?>> getEntities() {
+		return entities;
+	}
+}
---
+++ main/src/br/com/andorm/AndOrmException.java
@@ -0,0 +1,12 @@
+package br.com.andorm;
+
+
+public class AndOrmException extends RuntimeException {
+
+	private static final long	serialVersionUID	= 1L;
+	
+	public AndOrmException(String message) {
+		super(message);
+	}
+
+}
