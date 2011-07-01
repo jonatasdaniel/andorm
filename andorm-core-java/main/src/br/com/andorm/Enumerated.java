9f85dc543071e300249aa691eb4bb67316e84d6a
jonatasdaniel <=>
2011-06-30 21:31:40 -0300

    fixing enumerated annotation

--- andorm-core-java/main/src/br/com/andorm/Enumerated.java
+++ andorm-core-java/main/src/br/com/andorm/Enumerated.java
@@ -1,7 +1,14 @@
 package br.com.andorm;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 import br.com.andorm.types.EnumType;
 
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface Enumerated {
 
 	EnumType type() default EnumType.Ordinal;
