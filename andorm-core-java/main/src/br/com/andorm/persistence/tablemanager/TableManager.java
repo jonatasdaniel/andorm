8b20780a7bdcb210d69cd80d70b5692b6d92cc78
jonatasdaniel <=>
2011-09-04 23:24:59 -0300

    throwing exception in create method of table manager

--- andorm-core-java/main/src/br/com/andorm/persistence/tablemanager/TableManager.java
+++ andorm-core-java/main/src/br/com/andorm/persistence/tablemanager/TableManager.java
@@ -1,8 +1,13 @@
 package br.com.andorm.persistence.tablemanager;
 
+import java.text.MessageFormat;
+import java.util.ResourceBundle;
+
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
+import br.com.andorm.AndOrmException;
 import br.com.andorm.persistence.PersistenceManagerCache;
+import br.com.andorm.resources.ResourceBundleFactory;
 
 /**
  * 
@@ -13,6 +18,8 @@ import br.com.andorm.persistence.PersistenceManagerCache;
  */
 public final class TableManager {
 
+	private final ResourceBundle bundle = ResourceBundleFactory.get();
+	
 	private final PersistenceManagerCache cache;
 	private final SQLiteDatabase database;
 	
@@ -30,7 +37,7 @@ public final class TableManager {
 		try {
 			database.execSQL(createQuery);
 		} catch(SQLiteException e) {
-			
+			throw new AndOrmException(MessageFormat.format(bundle.getString("create_table_error"), entityClass.getCanonicalName(), e.getMessage()));
 		}
 	}
 	
