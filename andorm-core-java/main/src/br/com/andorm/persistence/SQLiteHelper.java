13f82cd626c5ee57fc85860cb877e8422c041a48
Tiago Emerick <tiagoemerick@gmail.com>
2013-01-08 11:18:48 -0200

    Calling TableManager to create tables

--- andorm-core-java/main/src/br/com/andorm/persistence/SQLiteHelper.java
+++ andorm-core-java/main/src/br/com/andorm/persistence/SQLiteHelper.java
@@ -3,35 +3,30 @@ package br.com.andorm.persistence;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import br.com.andorm.persistence.tablemanager.PropertyCreationQueryBuilder;
-import br.com.andorm.persistence.tablemanager.TableCreationQueryBuilder;
+import br.com.andorm.persistence.tablemanager.TableManager;
 
 /**
  * 
  * @author Tiago.Emerick
  * @since 07/01/2013
  * @version 0.1
- *
- * Class that implements SQLiteOpenHelper.
- * It's the standard way to create database and tables. 
+ * 
+ *          Class that implements SQLiteOpenHelper. It's the standard way to
+ *          create database and tables.
  */
 public class SQLiteHelper extends SQLiteOpenHelper {
 
-	private TableCreationQueryBuilder builder;
 	private PersistenceManagerCache cache;
-	
+
 	public SQLiteHelper(Context context, String databasePath, PersistenceManagerCache cache) {
 		super(context, databasePath, null, 1);
 		this.cache = cache;
 	}
-	
+
 	@Override
 	public void onCreate(SQLiteDatabase db) {
-		builder = new TableCreationQueryBuilder(cache, new PropertyCreationQueryBuilder());
-		for (Class<?> entityClass : cache.getAllEntities()) {
-			String table_create_sql_command = builder.build(entityClass);
-			db.execSQL(table_create_sql_command);
-		}
+		TableManager tm = new TableManager(cache, db);
+		tm.createAll();
 	}
 
 	@Override
